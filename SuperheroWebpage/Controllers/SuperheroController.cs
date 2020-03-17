84615e96ebe21c59962a6713eecd627bc8c6334c
Justin Haldeman <jmanh14@gmail.com>
2020-03-17 11:36:04 -0500

    Added database + added bootstrap theme

---
+++ SuperheroWebpage/Controllers/SuperheroController.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperheroWebpage.Data;
+using SuperheroWebpage.Models;
+
+namespace SuperheroWebpage.Controllers
+{
+    public class SuperheroController : Controller
+    {
+        private ApplicationDbContext db;
+        public SuperheroController(ApplicationDbContext context)
+        {
+            db = context;
+        }
+        // GET: Superhero
+        public ActionResult Index()
+        {
+            var superheroes = db.Superheroes;
+            return View(superheroes);
+        }
+
+        // GET: Superhero/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Superhero/Create
+        public ActionResult Create()
+        {
+            Superhero superhero = new Superhero();
+            return View(superhero);
+        }
+
+        // POST: Superhero/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Superhero superhero)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                db.Add(superhero);
+                db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Superhero/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Superhero/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- SuperheroWebpage/Data/ApplicationDbContext.cs
+++ SuperheroWebpage/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SuperheroWebpage.Models;
 
 namespace SuperheroWebpage.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheroes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
